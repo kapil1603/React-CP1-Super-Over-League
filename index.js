c99d9bdbb82978fea299c37a7464b497e5911d98
kapil1603 <kapilkumar1603@gmail.com>
2023-04-18 07:22:43 +0000

    React-CP1-Super-Over-League

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+  <div className="bgContainer">
+    <h1 className="head"> Super Over League </h1>
+    <div className="imgContainer">
+      <img
+        className="image1"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+
+      <img
+        className="image1"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
